c291e0af59a024f29e1f017578f1aa36e570cb87
maksim20pr <maksim20pr@gmail.com>
2025-12-09 23:36:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+  // to see how IntelliJ IDEA suggests fixing it.
+  Scanner in = new Scanner(System.in);
+  while (true) {
+        System.out.println("""
+                Hello world!
+                What kind of staff you need today?
+                0) See "Hello world!" and calculate area of a rectangle;
+                1) Option in development;
+                9) Exit;
+                Enter the option:""");
+        int option = in.nextInt();
+        if (option == 0) {
+            System.out.println("Hello world!");
+            System.out.println("Enter length:");
+            int a = in.nextInt();
+            System.out.println("Enter width:");
+            int b = in.nextInt();
+            System.out.println("Area of a rectangle: " + a * b);
+        } else if (option == 1) {
+            System.out.println("Not yet");
+        } else {
+            System.out.println("Wrong option!");
+        }
+  }
+}
